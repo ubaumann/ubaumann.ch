85d2148135cce3075a62b77316a8839aeac04198
Michel Bongard <michel_bongard@hotmail.com>
2021-12-14 09:34:28 +0100

    removed header

---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Skills.scss'
+
+const Skills = () => {
+    return (
+        <div className='Tile'>
+            
+        </div>
+    )
+}
+
+export default Skills
