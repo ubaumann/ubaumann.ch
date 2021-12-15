c01b76540ca569c6cd354c357fd0e36bf442b33c
Michel Bongard <michel_bongard@hotmail.com>
2021-12-15 15:02:33 +0100

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AboutMe from './components/AboutMe/AboutMe';
 import Skills from './components/Skills/Skills';
 import Experience from './components/Experience/Experience';
 import Projects from './components/Projects/Projects';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Skills />
       <Experience />
       <Projects />
+      <Footer />
     </div>
   );
 }
